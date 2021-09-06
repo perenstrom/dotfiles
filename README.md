1a752587bd4975c8346b1f84ff987459920f90ed
Per Enström <enstrom.per@gmail.com>
2021-09-06 14:30:05 +0200

    Update dax aliases and functions

--- README.md
+++ README.md
@@ -3,5 +3,10 @@ These are my dotfiles and install scripts for setting up a new system.
 
 ## Installation
 1. Run [zsh installation](/zsh)
-2. Run [Homebrew installation](/homebrew)
-3. Setup [Node JS](/nodejs)
+1. Run [Homebrew installation](/homebrew)
+1. Setup [Node JS](/nodejs)
+1. [Generate and add a SSH key to GitHub](https://docs.github.com/en/github/authenticating-to-github/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent)
+1. Clone a repo with the terminal to approve GitHub IP
+1. Install XCode from [App Store](https://apps.apple.com/us/app/xcode/id497799835?mt=12)
+1. Agree to XCode license with `sudo xcodebuild -license`
+1. Install [dax-cli-macos](https://github.com/bontouch/dax-cli-macos)
--- zsh/aliases-etc/bontouch.sh
+++ zsh/aliases-etc/bontouch.sh
@@ -7,6 +7,7 @@ alias gtsst="cd ~/Projects/Bontouch/Swish/support-tool"
 # Dax CLI
 DAX_PROJECT_BONTOUCH_AB="77904"
 DAX_PROJECT_SWISH_PORTAL="608785"
+DAX_PROJECT_SWISH_MISC="480727"
 
 function dax-custom-log() {
   PROJECT=$1
@@ -16,23 +17,47 @@ function dax-custom-log() {
 
   if [[ $DATESTRING == "" ]]; then
     dax log -p ${PROJECT} ${TIME} ${TEXT}
-  elif [[ $DATESTRING == "yesterday" ]]; then
-    DATE=$(date -j -v -1d '+%Y-%m-%d')
-    dax log -p ${PROJECT} -d ${DATE} ${TIME} ${TEXT}
   else
     dax log -p ${PROJECT} -d ${DATESTRING} ${TIME} ${TEXT}
   fi
 }
 
 function dax:webkul() {
-  dax-custom-log 77904 1:00 "#ca-web Web-KUL" $1
+  echo "Logging 1h for Bontouch AB: #ca-web Web-KUL"
+  dax-custom-log ${DAX_PROJECT_BONTOUCH_AB} 1h "#ca-web Web-KUL" $1
 }
 function dax:webfika() {
-  dax-custom-log 77904 0:30 "#ca-web Web-fika" $1
+  echo "Logging 30min for Bontouch AB: #ca-web Web-fika"
+  dax-custom-log ${DAX_PROJECT_BONTOUCH_AB} 30m "#ca-web Web-fika" $1
 }
 function dax:cooking() {
-  dax-custom-log 77904 0:30 "#manager-sync" $1
+  echo "Logging 30min for Bontouch AB: #manager-sync"
+  dax-custom-log ${DAX_PROJECT_BONTOUCH_AB} 30m "#manager-sync" $1
 }
 function dax:friday-demo() {
-  dax-custom-log 77904 1:00 "#friday-demo" $1
+  echo "Logging 1h for Bontouch AB: #friday-demo"
+  dax-custom-log ${DAX_PROJECT_BONTOUCH_AB} 1h "#friday-demo" $1
+}
+function dax:swish-demo() {
+  echo "Logging 30min for Swish - Misc: #internal-demo"
+  dax-custom-log ${DAX_PROJECT_SWISH_MISC} 30m "#internal-demo" $1
+}
+function dax:swish-portal() {
+  echo "Logging ${1} for Swish - Portal: ${2}"
+  dax-custom-log ${DAX_PROJECT_SWISH_PORTAL} $1 $2 $3
+}
+function dax:swish-misc() {
+  echo "Logging ${1} for Swish - Misc: ${2}"
+  dax-custom-log ${DAX_PROJECT_SWISH_MISC} $1 $2 $3
+}
+
+alias dax:today="dax entries --projects all"
+function dax:yesterday() {
+  DATE=$(date -j -v -1d '+%w')
+  if [[ $DATE == '0' ]] || [[ $DATE == '6' ]]; then
+    NEW_DATE=$(date -j -v -friday '+%Y-%m-%d')
+    dax entries --projects all --date ${NEW_DATE}
+  else
+    dax entries --projects all --date yesterday
+  fi
 }
