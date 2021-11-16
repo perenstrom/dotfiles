f27f3d389fa562f691f27afff67b50b9518d78ad
Per Enström <enstrom.per@gmail.com>
2021-11-16 13:17:21 +0100

    Update brew.sh

--- homebrew/brew.sh
+++ homebrew/brew.sh
@@ -1,3 +1,6 @@
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 # Add brew to path for this session
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
