32dfef8a9227448aa8503266a41f09f2744b7732
Per Enström <enstrom.per@gmail.com>
2021-11-17 10:16:55 +0100

    Add a lot of apps to brew installation

--- homebrew/brew.sh
+++ homebrew/brew.sh
@@ -22,3 +22,25 @@ brew install vercel-cli
 # Install NVM
 brew install nvm
 mkdir ~/.nvm
+
+# Install apps
+brew install --cask gitkraken
+brew install --cask slack
+brew install --cask firefox
+brew install --cask google-chrome
+brew install --cask visual-studio-code
+brew install --cask notion
+brew install --cask discord
+brew install --cask enpass
+brew install --cask zoom
+brew install --cask dropbox
+brew install --cask breaktimer
+brew install --cask bettertouchtool
+brew install --cask cleanshot
+brew install --cask microsoft-teams
+brew install --cask affinity-photo
+brew install --cask dash
+brew install --cask plex
+brew install --cask postman
+brew install --cask vlc
+brew install --cask cyberduck
---
+++ system/appStore/README.md
@@ -0,0 +1,5 @@
+Install the following apps from the app store:
+* PasteBot
+* Plume - Light Menubar Note
+* Screenbrush
+* Magnet
